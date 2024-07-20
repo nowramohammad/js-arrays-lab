32fe01185b593dda2b2af6a4e187cecefe4108a2
User Name <nowramohammad@Nowras-MacBook-Pro.local>
2024-07-20 14:20:26 -0500

    adding files

---
+++ app.js
@@ -0,0 +1,8 @@
+/* Exercise 1 */
+const foods = [];
+console.log("Exercise 1 Result:", foods);
+
+/* Exercise 2 */
+ foods.push("Pizza", "cheeseburger");
+ console.log("Exercise 2 Result:", foods);
+
